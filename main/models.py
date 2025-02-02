78a2c80979934a00fed692a092dd8ed3b3cc4824
itisanhad <anhadpreetadoresapples@helpusdefend.com>
2025-02-02 13:14:33 -0500

    User login

--- main/models.py
+++ main/models.py
@@ -1,28 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin
-from manager import CustomUserModel
+from .manager import CustomUserModel
 from django.utils import timezone
 
-class User (AbstractBaseUser):
-    email = models.EmailField(unique=True)
-    first_name = models.CharField(max_length=30, blank=True)
-    last_name = models.CharField(max_length=30, blank=True)
-    date_joined = models.DateTimeField(default=timezone.now)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    preferences = models.JSONField(default=dict, blank=True)  # Example: preferred sizes, brands
-    wishlist = models.ManyToManyField('products.Product', related_name='wishlisted_by', blank=True)
-    payment_methods = models.JSONField(default=list, blank=True)  # Example: saved card details
+class User(AbstractBaseUser,PermissionsMixin):
+        email = models.EmailField(unique=True)
+        first_name = models.CharField(max_length=30, blank=True)
+        last_name = models.CharField(max_length=30, blank=True)
+        date_joined = models.DateTimeField(default=timezone.now)
+        is_active = models.BooleanField(default=True)
+        is_staff = models.BooleanField(default=False)
+        preferences = models.JSONField(default=dict, blank=True)  # Example: preferred sizes, brands
+        payment_methods = models.JSONField(default=list, blank=True)  # Example: saved card details
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+        USERNAME_FIELD = 'email'
+        REQUIRED_FIELDS = []
 
-    objects = CustomUserModel()
+        objects = CustomUserModel()
 
-    def __str__(self):
-        return self.email
+        def __str__(self):
+            return self.email
+    
 
-# Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=20)
@@ -40,20 +39,20 @@ class Product(models.Model):
 
 class Order(models.Model):
     STATUS_CHOICES = [
-        ('1', 'Pending'),
-        ('2', 'Shipped'),
-        ('3', 'Delivered'),
-        ('4', 'Cancelled')
-    ],
+        ('Pending', 'Pending'),
+        ('Shipped', 'Shipped'),
+        ('Delivered', 'Delivered'),
+        ('Cancelled', 'Cancelled')
+    ]
     PAYMENT_STATUS_CHOICES = [
-    ('1', 'Paid'),
-    ('2', 'Unpaid'),
+    ('Paid', 'Paid'),
+    ('Unpaid', 'Unpaid'),
     ] 
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     products = models.ManyToManyField(Product)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    order_status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='1')
-    payment_status = models.CharField(max_length=20, choices=PAYMENT_STATUS_CHOICES, default='2')
+    order_status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
+    payment_status = models.CharField(max_length=20, choices=PAYMENT_STATUS_CHOICES, default='Unpaid')
     payment_method = models.CharField(max_length=50)
     shipping_address = models.JSONField()
     tracking_number = models.CharField(max_length=255, blank=True, null=True)
@@ -68,7 +67,6 @@ class Order(models.Model):
         product = models.ForeignKey(Product, on_delete=models.CASCADE)
         rating = models.PositiveIntegerField()
         comment = models.TextField(blank=True)
-        images = models.JSONField(default=list, blank=True)
         created_at = models.DateTimeField(auto_now_add=True)
         verified_purchase = models.BooleanField(default=False)
         likes = models.PositiveIntegerField(default=0)
@@ -76,3 +74,4 @@ class Order(models.Model):
         def __str__(self):
             return f"Review by {self.user.username} for {self.product.name}"
     
+
--- main/views.py
+++ main/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.views import View
+from django.http import JsonResponse
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth import authenticate,login
+
+from .models import User
+
+class User_Register(View):
+    def post(self,request):
+        email_id = request.POST.get['emailid'],
+        password = request.POST.get['password'],
+        first_name = request.POST.get['first_name'],
+        last_name = request.POST.get['last_name']
+        if not all([email_id,password,first_name,last_name]):
+            return redirect('/login/')
+        if User.objects.filter(email=email_id).exists():
+            return JsonResponse({'error':'Email ID already exists'},status=404)
+        usr = User.objects.create(email=email_id,password=make_password(password),first_name=first_name,last_name=last_name)
+        usr.save()
+        return JsonResponse({'error':'User created successfully'})
+
+class User_Login(View):
+    def post(self,request):
+        email_id = request.POST.get('emailid')
+        password = request.POST.get('password')
+        if not email_id and password:
+            return JsonResponse({'error':'Email or Password is required'})
+        user = authenticate(request,email_id,password)
+        if user is None:
+            return JsonResponse({'error':'Incorrect Email ID or Password'})
+        else:
+            login(email_id,password)
+            return redirect ('/home')
+
+
+
+
 
 # Create your views here.
