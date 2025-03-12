74b2424d096795bacef25f3b1d1a15c52d415760
“Harsimranjit <hm.harsimran@gmail.com>
2025-03-11 21:56:07 -0400

    View cart URL fixed

--- products/urls.py
+++ products/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('product_detail/<int:id>', views.Product_Details.as_view()),
     path('search_query',Result.as_view()),
     path('add_to_cart',views.Add_to_Cart.as_view()),
-    path('product_detail/view_cart',views.Cart_View.as_view()),
+    path('view_cart',views.Cart_View.as_view()),
     path('add_to_favourites',views.Favourites_product.as_view()),
     path('learning',Test.as_view())
 ]
